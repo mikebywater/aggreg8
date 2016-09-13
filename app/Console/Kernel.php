1a0b609e355ecb90ea913380d812601d72381e00
Mike Bywater <mike.bywater@enta.net>
2016-09-13 22:04:22 +0100

    move to hourly updaetes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('UpdateArticlesCommand')->everyThirtyMinutes();
+        $schedule->command('UpdateArticlesCommand')->hourly();
     }
 }
